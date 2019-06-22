9f0744db06abcbdd900f72b3ec0fcf11dec81838
dupin <halifaxpj@gmail.com>
2019-06-22 05:13:48 +0300

    chore: Set Up Erwin Project Files
    This will commit the initial files needed for setup of erwin project.
    The project will use spring-boot and java 8

---
+++ src/main/java/com/dupin/erwin/erwinapp/ErwinAppApplication.java
@@ -0,0 +1,13 @@
+package com.dupin.erwin.erwinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErwinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErwinAppApplication.class, args);
+	}
+
+}
