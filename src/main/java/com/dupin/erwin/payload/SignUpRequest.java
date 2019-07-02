5208a25f74857ba716404662f5f7a2e954c1156e
dupin <halifaxpj@gmail.com>
2019-07-03 01:25:10 +0300

    feat: Add Payload That Will Be Handled By Authentication Controllers
    To add the signup request, login request, api response and
    jwtauthentication response payloads that are to be used by the APIs.

---
+++ src/main/java/com/dupin/erwin/payload/SignUpRequest.java
@@ -0,0 +1,54 @@
+package com.dupin.erwin.payload;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 3, max = 23)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 3, max = 23)
+    private String lasttName;
+
+    @NotBlank
+    @Size(max = 37)
+    private String email;
+
+    @NotBlank
+    @Size(min = 7, max = 31)
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasttName() {
+        return lasttName;
+    }
+
+    public void setLasttName(String lasttName) {
+        this.lasttName = lasttName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
