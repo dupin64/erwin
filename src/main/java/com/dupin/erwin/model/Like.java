a1cbdb8cf74365e8f4921985d5da32c0c5082ebe
dupin <halifaxpj@gmail.com>
2019-07-05 20:03:58 +0300

    feat: Add Model Class For Likes
    Added a model class to hold information on the movies users like. It
    will hold foreign keys from users and movies tables

---
+++ src/main/java/com/dupin/erwin/model/Like.java
@@ -0,0 +1,52 @@
+package com.dupin.erwin.model;
+
+/*
+*  Model to hold info on what movies user likes
+* */
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "likes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "movie_id",
+                "user_id"
+        })
+})
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movies movie;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movies getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movies movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
