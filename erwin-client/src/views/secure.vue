f6eb052e69c8fcc5961f49c4ff32aaef8611576d
dupin <halifaxpj.com>
2019-07-13 02:55:26 +0300

    feat: Add A Web Client
    This is to distinguish the backend from the front end. The front end
    will be using javascript obviously. I chose to go with vue js because of
    its relative ease to learn compared to the rest. This is the initial
    setup.

---
+++ erwin-client/src/views/secure.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="secure">
+        <h1>Erwin Home</h1>      
+        <p>
+            Welcome to Erwin
+
+        </p>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'secure',
+    data(){
+        return {};
+    }
+}
+</script>
+
+<style scoped>
+ #secure{
+     background-color: #ffffff;
+     border: 1px solid #cccccc;
+     padding: 20px;
+     margin-top: 10px;
+     
+ }
+</style>
+
