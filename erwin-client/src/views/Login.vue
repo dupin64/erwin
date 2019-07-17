7fafcf1cc13fad4d93c40919a90e21b04a843440
dupin <halifaxpj.com>
2019-07-18 02:57:16 +0300

    feat: Add Login Template
    Added a simple Login template. Still not showing the uername and
    password fields though. Will figure that out next.

---
+++ erwin-client/src/views/Login.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="login">
+        <input type="text" name="username" id="username-field" placeholder="Username">
+        <input type="password" name="password" id="password-fielc" placeholder="Password">
+    </div>
+</template>
