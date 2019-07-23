f27558e73eff34eb2db15868a53196ad145b6e95
dupin <halifaxpj.com>
2019-07-23 03:06:20 +0300

    feat: Add Login Form
    Adds a login component. Has basic username and password input and data
    binding using v-model directive. Currently the method just prints to
    console the entered username and password as verification that the form
    behaves.

--- erwin-client/src/views/Login.vue
+++ erwin-client/src/views/Login.vue
@@ -3,20 +3,32 @@
         <!-- <input type="text" name="username" id="username-field" placeholder="Username" v-model="username">
         <input type="password" name="password" id="password-fielc" placeholder="Password" v-model="password">
     </div> -->
+    <form id="login-form" @submit.prevent="login" >
         username: <br>
-        <input type="text" name="username"><br>
+        <input v-model="username" type="text" name="username"><br>
         password: <br>
-        <input type="password" name="password">
-    </div>
+        <input v-model="password" type="password" name="password">
+        <br>
+        <button type="submit">Sign in </button>
+    </form>
+         </div>
 </template>
 
 <script>
 export default {
-   data(){
-       return {
-           username: ''
+    name: 'Login',
+    data(){
+       return{
+           username: '',
+           password: ''
        }
-   }
+    },
+    methods: {
+       login () {
+           console.log(this.username);
+           console.log(this.password);
+       }
+    }
     
 }
 </script>
