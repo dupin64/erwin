793fd71450b4328a9c01a8efc65919a4329ffc6a
dupin <halifaxpj.com>
2019-07-26 02:21:05 +0300

    style: Add CSS Styling To Login Component
    basically adds some bootstrap styling. The autofocus is not popping up
    as well as the input areas are still misaligned. Will fix them soon

--- erwin-client/src/views/Login.vue
+++ erwin-client/src/views/Login.vue
@@ -1,15 +1,13 @@
 <template>
-    <div class="container">
-        <!-- <input type="text" name="username" id="username-field" placeholder="Username" v-model="username">
-        <input type="password" name="password" id="password-fielc" placeholder="Password" v-model="password">
-    </div> -->
-    <form id="login-form" @submit.prevent="login" >
-        username: <br>
-        <input v-model="username" type="text" name="username"><br>
-        password: <br>
-        <input v-model="password" type="password" name="password">
-        <br>
-        <button type="submit">Sign in </button>
+    <div class="login-wrapper border border-light">
+       <!-- @submit.prevent directive actually points the usual submit action to the login method we defined -->
+    <form class="form-signin" id="login-form" @submit.prevent="login" >
+        <h2 class="form-signin-heading"> Login to Erwin</h2>
+        <label for="inputUserName" class="sr-only">username</label>
+        <input v-model="username" type="text" id="inputUserName" placeholder="Username" required autofocus>
+        <label for="inputPassword" class="sr-only"> password</label>
+        <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">LOGIN </button>
     </form>
          </div>
 </template>
@@ -32,4 +30,49 @@ export default {
     
 }
 </script>
+
+<style scoped>
+.login-wrapper {
+    background: #ffffff;
+    width: 70%;
+    margin: 12% auto;
+}
+
+.form-signin {
+    max-width: 330px;
+    padding: 10% 15px;
+    margin: 0 auto;
+    font-weight: normal;
+}
+
+.form-signin .form-signin-heading {
+    margin-bottom: 10px;
+}
+
+.form-signin .form-control {
+    position: relative;
+    height: auto;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 10px;
+    font-size: 16px;   
+}
+
+.form-signin .form-control:focus {
+    z-index: 2;
+}
+
+.form-signin input[type="text"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+}
+</style>
+
 
