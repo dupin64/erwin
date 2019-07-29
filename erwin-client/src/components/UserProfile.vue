0ef4e6d1637d4ecc6b0a98fa0b15ca82acdb934c
dupin <halifaxpj.com>
2019-07-29 23:35:19 +0300

    feat: Add A UserProfile Template
    Did some basic design of a user profile. Experimented with card
    component of bootstrap vue. It only has hardcoded data for now.

---
+++ erwin-client/src/components/UserProfile.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <b-card
+            no-body
+            style="max-width: 30rem;"
+            img-src="../assets/user-avatar.png"
+            img-alt="user-avatar"
+            img-top=""
+        >
+            <h4 slot="header">user profile</h4>
+
+            <b-card-body>
+                <b-card-title>Jon Dow</b-card-title>
+                <b-card-text>
+                    username
+                </b-card-text>
+                <b-card-text>
+                    email address
+                </b-card-text>
+                <b-card-text>
+                    date joined
+                </b-card-text>
+
+            </b-card-body>
+
+            <b-card-body>
+                <a href="#">go to shelf</a>
+                <a href="#">go to library</a>
+            </b-card-body>
+
+        </b-card>
+
+    </div>
+</template>
